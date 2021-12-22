77f473da4f94fca500e9b6f7affd5b8585514a06
Juan Chicaval <juanchicaval@gmail.com>
2021-12-22 20:32:02 -0300

    nuevos cambios js

--- js/main.js
+++ js/main.js
@@ -9,15 +9,21 @@ function Producto (id, tipo, cantidad, precio) {
 let producto1 = new Producto ( 1,"riñonera", 50, 1000);
 let producto2 = new Producto ( 2,"buff", 10, 500);
 let producto3 = new Producto ( 3,"magnesiera", 7, 1200)
+let producto4 = new Producto ( 4,"cuellitos", 15, 400)
 /*Arrays */
-const listaDeProductos = [producto1, producto2, producto3];
-//método push
-listaDeProductos.push(new Producto( 4, "cuellitos", 15, 400));
+/*Arrays */
+const listaDeProductos = [producto1, producto2, producto3, producto4];
+// creación del listado de prodcutos en el html 
 for(const producto of listaDeProductos){
-    console.log(producto)
+    let nuevodiv = document.createElement("div");
+    nuevodiv.innerHTML = `<h1> ID: ${producto.id}</h1>
+                          <h2> Producto: ${producto.tipo}</h2>
+                          <h3> $ ${producto.precio}</h3>
+                          <h3> $ ${producto.cantidad}</h3>`
+    document.body.appendChild(nuevodiv);
 }
 //método length para ir viendo cuantos productos hay en mi array
-console.log(listaDeProductos.length);
+//console.log(listaDeProductos.length);
 /*Acá voy a codear un método de busqueda ya que tengo pensado en sumarle a cada categoría (cuellito,riñonera,etc.)
  más productos y diferenciarlos por motivos de estampado para sumarle mas contenido. Por ahora solo lo dejo planteado después cuando
  sume los productos le agregare mas funcionalidad*/
@@ -31,51 +37,89 @@ console.log(listaDeProductos.length);
 console.log(buscador4);
 
 //menu para realizar acciones 
-// función para preguntar stock y precio de los productos
-let eleccion = prompt("ingrese el producto que quiere para saber su precio y stock. Seleccione entre riñonera, buff o magnesiera. 1 para salir");
-while (eleccion != 1) {
-    switch (eleccion) {
-        case "riñonera":
-            alert(producto1.stock());
+const menu = () =>{
+    let opcion = parseInt(prompt(`Ingrese la opcion desea:
+    1-preguntar stock
+    2-Ordenar por menor precio
+    3-ordenar por mayor stock
+    4-Salir`))
+
+    switch (opcion) {
+        case 1:
+            elegir()
             break;
-        case "buff":
-            alert(producto2.stock());
+        case 2:
+            alert(precioM())
             break;
-        case "magnesiera":
-            alert(producto3.stock());
+        case 3:
+            alert(stockM())
             break;
-        default: 
+        default:
+            alert("Muchas gracias por visitar nuestra página")
             break;
     }
 }
+menu()
+
+function elegir(){
+    let eleccion = parseInt(prompt("ingrese el producto que quiere para saber su precio y stock. Seleccione entre 1 riñonera, 2 buff o  3 magnesiera 4 cuellitos"))
+            switch (eleccion) {
+                case 1:
+                    alert(producto1.stock());
+                    break;
+                case 2:
+                    alert(producto2.stock());
+                    break;
+                case 3:
+                    alert(producto3.stock());
+                    break;
+                case 3:
+                    alert(producto3.stock());
+                    break;
+                case 4:
+                    alert(producto4.stock());
+                    break;
+        
+                    
+            }
+        }
+
+
+
 
 
 //ordenador por menor precio
-const precioMenor = listaDeProductos.sort((a, b) => {
-    let precioA = a.precio;
-    let precioB = b.precio;
-    if (precioA == precioB){
-        return 0;
-    }
-    if (precioA < precioB){
-        return -1;
-    }
-    return 0
+function precioM() {
+    const precioMenor = listaDeProductos.sort((a, b) => {
+        let precioA = a.precio;
+        let precioB = b.precio;
+        if (precioA == precioB){
+            return 0;
+        }
+        if (precioA < precioB){
+            return -1;
+        }
+        return 0
+        
 }
-);
-alert(precioMenor);
+  )  
+  alert(precioMenor)
+}
 
 //Ordenador por mayor stock 
-const stockMayor = listaDeProductos.sort((a, b) => {
-    let stockA = a.cantidad;
-    let stockB = b.cantidad;
-    if (stockA == stockB){
-        return 0;
+function stockM() {
+    const  stockMayor = listaDeProductos.sort((a, b) => {
+        let stockA = a.cantidad;
+        let stockB = b.cantidad;
+        if (stockA == stockB){
+            return 0;
+        }
+        if (stockA > stockB){
+            return -1;
+        }
+        return 0
     }
-    if (stockA > stockB){
-        return -1;
-    }
-    return 0
+    );
+    alert(stockMayor)
 }
-);
-alert(stockMayor);
+
